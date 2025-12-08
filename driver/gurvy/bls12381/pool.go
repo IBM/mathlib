14136cf7f33c038bd59d0f3cc9b9b11c7a39b1ab
Angelo De Caro <adc@zurich.ibm.com>
2025-12-08 13:28:52 +0100

    better Mul2
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

---
+++ driver/gurvy/bls12381/pool.go
@@ -0,0 +1,35 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package bls12381
+
+import (
+	"sync"
+
+	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
+)
+
+// G1Jacs is a shared *bls12381.G1Jac{} memory pool
+var G1Jacs g1JacPool
+
+var _g1JacPool = sync.Pool{
+	New: func() interface{} {
+		return new(bls12381.G1Jac)
+	},
+}
+
+type g1JacPool struct{}
+
+func (g1JacPool) Get() *bls12381.G1Jac {
+	return _g1JacPool.Get().(*bls12381.G1Jac)
+}
+
+func (g1JacPool) Put(v *bls12381.G1Jac) {
+	if v == nil {
+		panic("put called with nil value")
+	}
+	_g1JacPool.Put(v)
+}
